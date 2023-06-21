ab6a6d29a1dbd3f9e79f6516feb77cd3bb25a313
Dmitry Uvarov <diman_niks@mail.ru>
2023-06-21 22:57:45 +0300

    feat: footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+import styles from './footer.module.css';
+
 export default function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <Link className={styles.link} href="/faq">
+        Вопросы-ответы
+      </Link>
+      <Link className={styles.link} href="/about">
+        О нас
+      </Link>
+    </footer>
+  );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import styles from './header.module.css';
 import Logo from '../../assets/icons/basket.svg';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Билетопоиск</div>
+      <Link href="/">
+        <div className={styles.logo}>Билетопоиск</div>
+      </Link>
       <div className={styles.basket}>
         <Logo className={styles.basket__icon} />
       </div>
